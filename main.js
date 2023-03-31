7f0190b426d7296c7bbac9c61cffba247d5bf33e
Léopoldine <lhallynck@gmail.com>
2023-03-31 12:49:27 +0800

    Clean up indentation in modal html

--- main.js
+++ main.js
@@ -5,23 +5,23 @@ class Modal extends HTMLElement {
 		this._modal
 		this.attachShadow({ mode: "open" })
 		this.shadowRoot.innerHTML = `
-        <link rel="stylesheet" href="main.css" />
-        <button>Click me</button>
-				<p id="output"></p>
-        <div class="modal">
-            <div class="modal-content">
-                <div class="modal-body">
-					<div class="modal-body-content">
-						<p></p>
-					</div>
-					<div class="buttons">
-						<button class="confirmBtn" id="confirmBtn" value="Yes">Yes</button>
-						<button id="cancel" value="Cancel">Cancel</button>
-					</div>
-                </div>
+      <link rel="stylesheet" href="main.css" />
+      <button>Click me</button>
+      <p id="output"></p>
+      <div class="modal">
+        <div class="modal-content">
+          <div class="modal-body">
+            <div class="modal-body-content">
+              <p></p>
             </div>
+            <div class="buttons">
+              <button class="confirmBtn" id="confirmBtn" value="Yes">Yes</button>
+              <button id="cancel" value="Cancel">Cancel</button>
+            </div>
+          </div>
         </div>
-        `
+      </div>
+      `
 		this.output = this.shadowRoot.querySelector("#output")
 		this.button = this.shadowRoot.querySelector("button")
 		this.confirm = this.shadowRoot.querySelector("#confirmBtn")
